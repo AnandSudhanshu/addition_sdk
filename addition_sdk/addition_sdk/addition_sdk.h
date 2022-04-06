1b0de6b7e28a7b973faf7a4af9a7b1df93c54ab2
Anand Kumar <anand.kumar2@comviva.com>
2022-04-06 13:52:37 +0530

    addition method

---
+++ addition_sdk/addition_sdk/addition_sdk.h
@@ -0,0 +1,18 @@
+//
+//  addition_sdk.h
+//  addition_sdk
+//
+//  Created by Anand on 06/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for addition_sdk.
+FOUNDATION_EXPORT double addition_sdkVersionNumber;
+
+//! Project version string for addition_sdk.
+FOUNDATION_EXPORT const unsigned char addition_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <addition_sdk/PublicHeader.h>
+
+
